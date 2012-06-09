4b9103e9f7b109faa74226c708fb54605c37138f
Greg Winn <greg@winn.ws>
2012-06-09 13:01:26 -0400

    Version 0.3.0 - Added google option along with performance updates for Yahoo

--- lib/ystock.rb
+++ lib/ystock.rb
@@ -7,6 +7,7 @@
 # http://github.com/gregwinn/ystock
 #
 require 'cgi'
+require 'json'
 require 'net/http'
 module Ystock
     def ensure_unique(name)
@@ -15,47 +16,100 @@ module Ystock
         end while self.class.exists?(name => self[name])
     end
     
-     @@service_uri = "http://download.finance.yahoo.com/d/quotes.csv"
-     
-     # Ystock.find(['stock', 'stock'])
-	def self.find(args)
-        output = Hash.new
-        args.each do |stock|
-            s = send_request(stock.to_s)
-            a = s.chomp.split(",")
-			
-            output[stock.to_sym] = {:symbol => stock.to_s,
-             :price => a[0],
-             :change => a[1],
-             :volume => a[2]}
-
-        end
-        return output
+	@@service_uri = "http://download.finance.yahoo.com/d/quotes.csv"
+	
+	# => Ystock::Google
+	class Google
+		@@google_service = "http://finance.google.com/finance/info"
+		# => Ystock::Google.find(["aapl", "goog"])
+		def self.find(args)
+			last_stock = args.last
+			stock_string = ""
+			args.each do |stock|
+				if last_stock == stock
+					stock_string += stock
+				else
+					stock_string += stock + ","
+				end
+			end
+			# => Send Request to get quote then format
+			format(send_request(stock_string))
+		end
+		
+		def self.get_quote(arg)
+			# => One stock
+			format(send_request(arg))
+		end
+		
+		def self.format(results)
+			output = Hash.new
+			results.each do |result|
+				output[result["t"].to_sym] = {
+					:symbol => result["t"],
+					:market => result["e"],
+					:price => result["l"],
+		            :change => result["c"]
+				} 
+			end
+			return output
+		end
+		
+		def self.send_request(args)
+			url = @@google_service + "?client=ig&q="
+			url += args
+			resp = Net::HTTP.get_response(URI.parse(url))
+			data = resp.body.gsub("//", "")
+			# => Result = Hash
+			result = JSON.parse(data)
+		end
 	end
      
+    # Ystock.find(['stock', 'stock'])
+	def self.find(args)
+		stock_string = ""
+		last_stock = args.last.to_s
+        args.each do |stock|
+			if last_stock == stock.to_s
+				stock_string += stock.to_s
+			else
+				stock_string += stock.to_s + ","
+			end
+        end
+		format(send_request(stock_string))
+	end
+    
     def self.get_stock(stock)
-        output = Array.new
+        output = Hash.new
         s = send_request(stock)
         a = s.chomp.split(",")
-        output = {:symbol => stock,
+        output[stock.to_sym] = {:symbol => stock,
                 :price => a[0],
                 :change => a[1],
                 :volume => a[2]
                 }
         return output
     end
+	
+	def self.format(results)
+		output = Hash.new
+		results.each_line("\n") do |row|
+			stockdata = row.split(",")
+			stockdata[3] = stockdata[3].gsub("\r\n", "").gsub('"', '')
+			output[stockdata[3].to_sym] = {:symbol => stockdata[3],
+	         :price => stockdata[0],
+	         :change => stockdata[1],
+	         :volume => stockdata[2]}
+		end
+        return output
+	end
 
-    def self.send_request(*args)
-        completed_path = @@service_uri + construct_args(*args)
+    def self.send_request(args)
+        completed_path = @@service_uri + "?f=l1c1v1s&s=" + args
         uri = URI.parse(completed_path)
         response = Net::HTTP.start(uri.host, uri.port) do |http|
             http.get completed_path
         end
         return response.body
-     end
-
-     def self.construct_args(*args)
-        path = "?f=l1c1v&s=" + args.map{|x| CGI.escape(x)}.join(",")
      end
     
 end
--- ystock.gemspec
+++ ystock.gemspec
@@ -2,21 +2,21 @@
 
 Gem::Specification.new do |s|
   s.name = %q{ystock}
-  s.version = "0.2.5"
+  s.version = "0.3.0"
 
   s.required_rubygems_version = Gem::Requirement.new(">= 1.2") if s.respond_to? :required_rubygems_version=
   s.authors = ["Greg Winn"]
-  s.date = %q{2012-05-18}
-  s.description = %q{Get stock information from Yahoo Finance}
+  s.date = %q{2012-06-09}
+  s.description = %q{Get stock information from Yahoo Finance and Google}
   s.email = %q{greg@winn.ws}
-  s.extra_rdoc_files = ["README.rdoc", "lib/ystock.rb"]
-  s.files = ["Manifest", "README.rdoc", "Rakefile", "init.rb", "lib/ystock.rb", "ystock.gemspec"]
+  s.extra_rdoc_files = ["README.markdown", "lib/ystock.rb"]
+  s.files = ["Manifest", "README.markdown", "Rakefile", "init.rb", "lib/ystock.rb", "ystock.gemspec"]
   s.homepage = %q{http://github.com/gregwinn/ystock}
   s.rdoc_options = ["--line-numbers", "--inline-source", "--title", "Ystock", "--main", "README.rdoc"]
   s.require_paths = ["lib"]
   s.rubyforge_project = %q{ystock}
   s.rubygems_version = %q{1.5.0}
-  s.summary = %q{Get stock information from Yahoo Finance}
+  s.summary = %q{Get stock information from Yahoo Finance and Google}
 
   if s.respond_to? :specification_version then
     s.specification_version = 3

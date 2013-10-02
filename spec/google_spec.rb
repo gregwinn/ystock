bf94f8f993280466febd3a3c48a44a347613599d
Greg Winn <greg@winn.ws>
2013-10-02 12:30:23 -0500

    Updated paths for testing

--- spec/google_spec.rb
+++ spec/google_spec.rb
@@ -1,7 +1,8 @@
 # => Google Tests
-require "../ystock/ystock.rb"
+require "./lib/ystock/google.rb"
 describe Google, "#result" do
 	it "request fails if no args are provided" do
 		Ystock::Google.send_request(nil)
+		# => uninitialized constant Google (NameError)
 	end
 end

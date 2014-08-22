0209fbe6e6ead88643f06688caf69de36aea6e9d
Greg Winn <greg@winn.ws>
2014-08-21 22:06:00 -0500

    Gem spec date updated, yahoo tests updated for rspec

--- spec/yahoo_spec.rb
+++ spec/yahoo_spec.rb
@@ -5,11 +5,11 @@ describe Ystock do
 	context "service response" do
 		it "should retrieve stock symbol" do
 			stock = Ystock::Yahoo.quote("AAPL")
-			stock[:symbol].should eq("AAPL")
+			expect(stock[:symbol]).to include("AAPL")
 		end
 		it "should retrieve stock symbol for first stock" do
 			stock = Ystock::Yahoo.quote(["goog", "AAPL"])
-			stock[0][:symbol].should eq("GOOG")
+			expect(stock[0][:symbol]).to include("GOOG")
 		end
 	end
 end
--- ystock.gemspec
+++ ystock.gemspec
@@ -2,7 +2,7 @@ Gem::Specification.new do |s|
   s.name = 'ystock'
   s.version = "0.4.6"
   s.authors = ["Greg Winn"]
-  s.date = %q{2012-06-09}
+  s.date = %q{2014-08-21}
   s.description = %q{Grab stock information from Yahoo and Google Finance}
   s.email = %q{greg@winn.ws}
   s.extra_rdoc_files = ["README.markdown", "lib/ystock.rb"]
